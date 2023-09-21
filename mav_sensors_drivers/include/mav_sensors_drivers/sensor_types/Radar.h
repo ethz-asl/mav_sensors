d9afaaccc40e0beef8cb35f8eb7c9e9cd83ee522
Rik Bähnemann <brik@ethz.ch>
2023-09-21 15:51:59 +0200

    print_radar_detection

--- mav_sensors_drivers/include/mav_sensors_drivers/sensor_types/Radar.h
+++ mav_sensors_drivers/include/mav_sensors_drivers/sensor_types/Radar.h
@@ -7,6 +7,8 @@
 #include <cmath>
 #include <vector>
 
+#include <log++.h>
+
 #include "mav_sensors_core/sensor.h"
 
 namespace mav_sensors {
@@ -22,11 +24,24 @@ class Radar : public SensorType {
     float velocity{std::nanf("1")};
     int16_t snr{-1};
     int16_t noise{-1};
+    void print(const std::string& s = "") const {
+      LOG(I, s.c_str() << "x: " << x << " y: " << y << " z: " << z << " velocity: " << velocity
+                       << " snr: " << snr << " noise: " << noise);
+    }
   };
   std::vector<CfarDetection> cfar_detections;
   uint32_t hardware_stamp{0xFFFFFFFF};
   uint64_t unix_stamp_ns{0};
   typedef Radar ReturnType;
+
+  void print(const std::string& s = "") const {
+    LOG(I, s.c_str() << cfar_detections.size() << " radar detections:");
+    for (const auto& detection : cfar_detections) {
+      detection.print(s + "- ");
+    }
+    LOG(I, s.c_str() << "Hardware stamp: " << hardware_stamp);
+    LOG(I, s.c_str() << "Unix stamp: " << unix_stamp_ns);
+  }
 };
 
 }  // namespace mav_sensors
